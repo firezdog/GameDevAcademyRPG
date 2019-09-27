bc2e23879b5362df4f4e217a9eb25f43b98875e0
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-09-26 21:58:34 -0500

    first commit

---
+++ Assets/Scripts/StartBattle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartBattle : MonoBehaviour
+{
+    // Make GO persistent and load method to activate.
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        gameObject.SetActive(false);
+    }
+
+    // callback -- avoid duplicates in the title (and duplicate callbacks?) -- otherwise activate in battle.
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Title") {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+            Destroy(gameObject);
+        } else {
+            gameObject.SetActive(scene.name == "Battle");
+        }
+    }
+}
---
+++ Assets/Scripts/UnitStats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    public float health;
+    public float mana;
+    public float attack;
+    public float magic;
+    public float defense;
+    public float speed;
+}
