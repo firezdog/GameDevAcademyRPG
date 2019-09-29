5117dac4c780699d0595e781c76ee8a576d82f4a
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-09-28 21:54:45 -0500

    player can now trigger the battle scene

---
+++ Assets/SceneTester.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("t")) SceneManager.LoadScene("Test");
+    }
+}
---
+++ Assets/SpawnEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawnEnemy : MonoBehaviour
+{
+
+    [SerializeField] GameObject enemyEncounter;
+    private bool triggered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoad;
+    }
+
+    private void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        SceneManager.sceneLoaded -= OnSceneLoad;
+        if (triggered) Instantiate(enemyEncounter);
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            triggered = true;
+            DontDestroyOnLoad(gameObject);  
+            SceneManager.LoadScene("Battle");
+        } 
+    }
+
+}
