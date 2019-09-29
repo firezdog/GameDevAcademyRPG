0f1e1e920e62609aa9746a5015292e7994531349
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-09-29 17:01:09 -0500

    adding enemy spawning

---
+++ Assets/EnemyEncounter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyEncounter : MonoBehaviour
+{
+
+    [CreateAssetMenu]
+    class ComponentEnemy : ScriptableObject {
+        [SerializeField] GameObject enemyPrefab;
+        public GameObject EnemyPrefab { get => enemyPrefab; }
+        [SerializeField] private int numberToSpawn;
+        public int NumberToSpawn { get => numberToSpawn; }
+  }
+
+    [SerializeField] ComponentEnemy[] spawnList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemies();
+    }
+
+    void SpawnEnemies()
+    {
+        foreach(ComponentEnemy enemy in spawnList)
+        {
+            for (int i = 0; i < enemy.NumberToSpawn; i++)
+            {
+                Instantiate(enemy.EnemyPrefab, gameObject.transform);
+            }
+        }
+    }
+
+}
