9ceafa33af018a5aa71c7f3206557ada945c675c
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-10-03 16:21:16 -0500

    adding turn logic

---
+++ Assets/TurnSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TurnSystem : MonoBehaviour
+{
+
+    List<UnitStats> contestants;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetAllContestants();
+        print(contestants.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GetAllContestants()
+    {
+        contestants = new List<UnitStats>();
+        GameObject[][] units = new GameObject[2][];
+        units[0] = GameObject.FindGameObjectsWithTag("PlayerUnit");
+        units[1] = GameObject.FindGameObjectsWithTag("EnemyUnit");
+        foreach(GameObject[] unitGroup in units)
+        {
+            foreach(GameObject unit in unitGroup)
+            {
+                contestants.Add(unit.GetComponent<UnitStats>());
+            }
+        }
+    }
+
+    // get the next contestant to move
+    void NextTurn()
+    {
+    }
+
+}
